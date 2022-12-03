6de8d7a511922b8fbdeaca43c3b5fbdc5b9bf805
Silas Peters <silaspeters03@gmail.com>
2022-12-03 18:16:55 +0100

    Hersenspinsels

--- SudokuCracker/Program.cs
+++ SudokuCracker/Program.cs
@@ -1,9 +1,21 @@
-﻿// const string SudokuTestsPath = "Sudoku_puzzels_5.txt";
+﻿namespace SudokuCracker;
 
-namespace Program{
-    class Program {
-        static void Main(){
-            Console.WriteLine("Hello, silas");
-        }
+class Program
+{
+    const string SudokuTestsPath = "Sudoku_puzzels_5.txt";
+
+    static void Main()
+    {
+        Console.WriteLine("Hello, silas");
+    }
+
+    static void LoadSudoku()
+    {
+        // Dit is maar een random attempt
+        string input = "1 2 3 4 5 6 7 8 9";
+        var converted = input.Insert(9, "\n").Insert(18, "\n").Split().ToArray()
+            .Select(x =>
+                x.Select(y => int.Parse(x)))
+            .ToArray();
     }
 }
