f0c677c983a4e54c674098c2e5d869211e71a4c2
Silas Peters <silaspeters03@gmail.com>
2023-01-16 13:31:54 +0100

    Added P2

---
+++ P2/SudokuCracker/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace SudokuCracker;
+[DebuggerDisplay("{Value}, fixed: {IsFixed}")]
+public struct Tile
+{
+	public Tile(byte value, bool isFixed)
+	{
+		Value   = value;
+		IsFixed = isFixed;
+	}
+
+	// A custom equality comparer, used to compare tiles based on their contents
+	private sealed class ValueEqualityComparer : IEqualityComparer<Tile>
+	{
+		public bool Equals(Tile x, Tile y) => x.Value == y.Value;
+		public int GetHashCode(Tile obj) => obj.Value.GetHashCode();
+	}
+	public static IEqualityComparer<Tile> ValueComparer { get; } = new ValueEqualityComparer();
+
+	public byte Value   { get; set; }
+	public bool IsFixed { get; set; }
+
+	public override string ToString() => Value > 0 ? Value.ToString() : "-";
+}
