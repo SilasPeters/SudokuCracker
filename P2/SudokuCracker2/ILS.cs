22e8ddfbde60a028e7d1a88cfbda756e0a356f85
Silas Peters <silaspeters03@gmail.com>
2023-01-16 13:34:39 +0100

    Added P2

---
+++ P2/SudokuCracker2/ILS.cs
@@ -0,0 +1,109 @@
+namespace SudokuCracker;
+
+static class ILS{ // ILS -> Iterated Local Search
+    public static Sudoku search(Sudoku s, int platStepNum) {
+        int lowestHYet = 1000;
+        
+        int platCount = 0;
+        while (s.H > 0){      //stopconditie (h == 0)
+            if (!step(ref s)){ //klein plateau
+                ++platCount;
+                if (platCount >= 10){ //groot (genoeg) plateau
+                    for (var i = 0; i < platStepNum; i++){
+                        plateauStep(ref s); //S keer willekeurig swappen
+                    }
+                    platCount = 0;
+                    s.H = s.CalculateHeuristicValue();
+                        // plateuStep resets this, so we need to update it manually
+                }
+            } else platCount = 0; //geen plateau (meer)
+            
+            // Show progress, if made
+            if (s.H < lowestHYet) {
+                lowestHYet = s.H;
+                Console.WriteLine($"\nLowest heuristic value so far: {lowestHYet}");
+                Console.WriteLine(s);
+            }
+        }
+
+        return s;
+    }
+    /// <remarks>Resets the heuristic value of the sudoku to -1</remarks>
+    private static Sudoku plateauStep(ref Sudoku s) {
+        // Get random block
+        var randomBlockIndex = Random.Shared.Next(0, 9);
+        var randomBlock      = s.GetBlock((byte) randomBlockIndex);
+        // Get a non-fixed random tile pair to be swapped
+        var randomTileIndex1 = Random.Shared.Next(0, 9);
+        var randomTileIndex2 = Random.Shared.Next(0, 9);
+
+        if (randomTileIndex1 == randomTileIndex2)
+            return plateauStep(ref s); // Retry
+
+        // Translate index of a tile to coordinate of the BLOCK
+        int blockX = randomBlockIndex % 3 * 3,
+            blockY = randomBlockIndex / 3 * 3;
+        int ax = randomTileIndex1 % 3, ay = randomTileIndex1 / 3,
+            bx = randomTileIndex2 % 3, by = randomTileIndex2 / 3;
+        
+        // Don't swap a fixed tile
+        if (randomBlock.IsFixed(ax, ay) 
+         || randomBlock.IsFixed(bx, by))
+            return plateauStep(ref s); // Retry TODO: causes infinite recursion if a block is completely fixed
+        
+        // At this point, we found a valid swap. Swap!
+        // To do this properly, we need to translate the Block coordinates to Sudoku coordinates
+        ax += blockX; ay += blockY;
+        bx += blockX; by += blockY;
+        
+        s.Swap(ax, ay, bx, by, -1); // We did not determine the new heuristic value, thus reset it
+        return s;
+    }
+    
+    /// <summary>
+    /// Picks a random block and finds and performs the best swap possible in tha block, based on the heuristic value
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns>Whether a swap was performed</returns>
+    private static bool step(ref Sudoku s)
+    {
+        // Prepare data
+        var bestH            = s.H;
+        var bestSwap         = Array.Empty<int>();
+        var randomBlockIndex = Random.Shared.Next(0, 9);
+        var randomBlock      = s.GetBlock((byte) randomBlockIndex);
+        int blockX = randomBlockIndex % 3 * 3,
+            blockY = randomBlockIndex / 3 * 3;
+		
+        // Try all swaps
+        for (var i = 0; i < 8; i++) for (var j = i + 1; j < 9; j++) // Note that j starts at i + 1, to prevent duplicate tests
+        {
+            // Translate index of a tile to coordinate of the BLOCK
+            //     [0..8] -> (x, y)
+            //     Example: 4 -> (1, 1)
+            int ax = i % 3, ay = i / 3,
+                bx = j % 3, by = j / 3;
+            
+            if (randomBlock.IsFixed(ax, ay) || randomBlock.IsFixed(bx, by))
+                continue; // Don't try to swap a fixed value
+            
+            // Convert coordinates within block to coordinates within SUDOKU
+            ax += blockX; ay += blockY;
+            bx += blockX; by += blockY;
+
+            // Try the swap, store it if is the best one yet
+            var h = s.DetermineHeuristicChangeAfterSwap(ax, ay, bx, by, s.H);
+            if (h < bestH)
+            {
+                bestH    = h;
+                bestSwap = new [] { ax, ay, bx, by };
+            }
+        }
+        
+        // Apply best swap, if existing
+        if (!bestSwap.Any())
+            return false; // Indicate that no swap was made
+        s.Swap(bestSwap[0], bestSwap[1], bestSwap[2], bestSwap[3], bestH);
+        return true; // Indicate that a swap was made
+    }
+}
---
+++ P2/SudokuCracker2/Sudoku.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Diagnostics;
+using System.Text;
+
+namespace SudokuCracker;
+
+public class Sudoku
+{
+	public Sudoku (IReadOnlyList<byte> numbers)
+	{
+		// This constructor converts list of numbers to blocks
+		
+		for (var blockY = 0; blockY < 3; blockY++) for (var blockX = 0; blockX < 3; blockX++)
+		{
+			int x = 3 * blockX, // Determine the offset of each block
+				y = 3 * blockY;
+
+			// Generate tiles for the current block
+			var tiles = new Tile[3, 3];
+			for (var by = 0; by < 3; by++) for (var bx = 0; bx < 3; bx++)
+			{
+				// Generate tiles based on the input
+				var tileValue = numbers[(x + bx) + (y + by) * 9]; // Calculations compensate for the inputs syntax
+				tiles[bx, by] = new Tile(tileValue, tileValue != 0);
+			}
+			
+			_blocks[blockX, blockY] = new Block(tiles);
+		}
+
+		// Don't forget to calculate the initial heuristic value
+		H = CalculateHeuristicValue();
+	}
+
+	[DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+	private readonly Block[,] _blocks = new Block[3, 3];
+
+	public int H;
+
+	/// <summary>Gives a reference to the block, based on the given index (0 - 8)</summary>
+	/// <param name="index">0-based index, maximum of 8</param>
+	public Block GetBlock(byte index) {
+		if (index > 8)
+			throw new ArgumentOutOfRangeException(nameof(index), "Should be 0 til 8");
+		return _blocks[index % 3, index / 3];
+	}
+
+	public int CalculateHeuristicValue()
+	{
+		var h                         = 0;
+		// For every column and row, calculate its heuristic value and aggregate it.
+		for (var y = 0; y < 9; y++) h += _CalculateHeuristicValueOfEnumerable(GetRowEnumerable(y));
+		for (var x = 0; x < 9; x++) h += _CalculateHeuristicValueOfEnumerable(GetColumnEnumerable(x));
+
+		return h;
+	}
+
+	private static int _CalculateHeuristicValueOfEnumerable(IEnumerable<Tile> tiles)
+	{
+		var h     = 0;
+		var found = new bool[10];
+		
+		foreach (var tile in tiles)
+		{
+			if (found[tile.Value]) h += 1; // Value is a duplicate, this means that a number is missing
+			else found[tile.Value] = true; // Unique value, remember that we found it
+		}
+
+		return h;
+	}
+	
+	/// <summary>Determines the new heuristic value after a potential swap</summary>
+	/// <remarks>Assumes that both points, a and b, belong to the same block.</remarks>
+	public int DetermineHeuristicChangeAfterSwap(int ax, int ay, int bx, int by, int currentH)
+	{
+		// Gather relevant columns and rows
+		var columnA = GetColumnEnumerable(ax).ToList();
+		var rowA    = GetRowEnumerable(ay)	 .ToList();
+		var columnB = GetColumnEnumerable(bx).ToList();
+		var rowB    = GetRowEnumerable(by)	 .ToList();
+
+		// Remember the old values of the tiles before swapping
+		var oldA = columnA[ay];
+		var oldB = columnB[by];
+		
+		// Prepare data: remove the tiles to be swapped
+		columnA.RemoveAt(ay);
+		rowA.	RemoveAt(ax);
+		columnB.RemoveAt(by);
+		rowB.	RemoveAt(bx);
+
+		return currentH // Accumulate all changes in the heuristic value
+		     + getHeuristicChangeOf(columnA, oldA, oldB)
+		     + getHeuristicChangeOf(rowA,    oldA, oldB)
+		     + getHeuristicChangeOf(columnB, oldB, oldA)
+		     + getHeuristicChangeOf(rowB,    oldB, oldA);
+		
+		int getHeuristicChangeOf(IReadOnlyCollection<Tile> range, Tile old, Tile swapped)
+		{
+			bool oldValueWasDuplicate = range.Contains(old,     Tile.ValueComparer);
+			bool newValueIsDuplicate  = range.Contains(swapped, Tile.ValueComparer);
+
+			if (oldValueWasDuplicate)
+			{
+				// If the new value is not a duplicate, this means that we progress
+				return newValueIsDuplicate ? 0 : -1;
+			}
+			else
+			{
+				// If the new value is not a duplicate, this means that we LOSE progress
+				return newValueIsDuplicate ? 1 : 0;
+			}
+		}
+	}
+	///<remarks> swaps two tiles in the sudoku, assumes the two tiles are in the same block</remarks>
+	public void Swap(int ax, int ay, int bx, int by, int newH) {
+		// Get the block, and its coordinates
+		var (block, xOffset, yOffset) = GetBlockContaining(ax, ay);
+		// Swap, using coordinates relative from the block's
+		block.Swap(
+			ax - xOffset, // Assumes that both points are within the same block
+			ay - yOffset,
+			bx - xOffset,
+			by - yOffset);
+		H = newH;
+	}
+	
+	/// <returns> Returns whether the tile at (x,y) is a fixed tile</returns>
+	public bool IsFixed(int x, int y) {
+		var (block, _, _) = GetBlockContaining(x, y);
+		return block.IsFixed(x % 3, y % 3);
+	}
+
+	/// <returns>Returns the block containing (x,y), and the offset of that block</returns>
+	public (Block, int, int) GetBlockContaining(int x, int y) =>
+		(_blocks[x/3, y/3], x/3 * 3, y/3 * 3); 
+
+	private IEnumerable<Tile> GetColumnEnumerable(int x)
+	{
+		var blockX         = x / 3; // The x-index of the relevant blocks
+		var blockRelativeX = x % 3; // The x-index of the relevant column inside the blocks
+		for (var y = 0; y < 3; y++) // For every relevant block containing the specified column
+			foreach (var tile in _blocks[blockX, y].GetColumnEnumerable(blockRelativeX))
+				// For every tile in the specified column, within this block
+				yield return tile;
+	}
+
+	public IEnumerable<Tile> GetRowEnumerable(int y)
+	{ // Does the same exact thing as GetColumnEnumerable(int x), but horizontally
+		var blockY         = y / 3;
+		var blockRelativeY = y % 3;
+		for (var x = 0; x < 3; x++)
+			foreach (var tile in _blocks[x, blockY].GetRowEnumerable(blockRelativeY))
+				yield return tile;
+	}
+
+	public override string ToString() // This is where the ASCII magic happens, which is hard to explain
+	{
+		StringBuilder sb = new();
+		const int totalWidth = (9 * 2 - 1) + 4; // 9 numbers with spaces, minus the last space, plus 2 separating bars with 2 spaces
+
+		for (var y = 0; y < 9; y++)
+		{
+			foreach (var number in GetRowEnumerable(y))
+				sb.Append($"{number} ");
+
+			insertVertically("│ ");
+			
+			if (y != 8 && (y + 1) % 3 == 0) // Add horizontal pipes
+			{
+				sb.AppendLine();
+				sb.Append('─', totalWidth - 3);
+				insertVertically("┼─");
+			}
+			
+			sb.AppendLine();
+
+			void insertVertically(string seperator)
+			{
+				sb.Insert(sb.Length - 6 * 2, seperator);
+				sb.Insert(sb.Length - 3 * 2, seperator);
+			}
+		}
+		
+		return sb.ToString();
+	}
+}
