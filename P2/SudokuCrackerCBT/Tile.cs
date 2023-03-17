338fa7a99085cb9f48dd67a03ab71efab8281164
Silas Peters <silaspeters03@gmail.com>
2023-03-17 15:52:32 +0100

    Tile rework

--- P2/SudokuCrackerCBT/Tile.cs
+++ P2/SudokuCrackerCBT/Tile.cs
@@ -1,10 +1,10 @@
 ﻿namespace SudokuCrackerCBT;
 using System.Diagnostics;
 
-[DebuggerDisplay($"{{Value}}, fixed: {{IsFixed}}, domain: {{Constraint}}, backing: {{domain}}")]
+[DebuggerDisplay($"{{Value}}, fixed: {{IsFixed}}, backing: {{Constraint}}, backing: {{backing}}")]
 public struct Tile
 {
-	private short domain;
+	private short backing;
 	// The 9 Most-Significant (first) bits represent the constraints of the tile.
 	// The 1 bit after that resembles whether this tile was given/is fixed.
 	// The 4 Least-Significant (last) bits represent the current value of the tile.
@@ -17,23 +17,23 @@ public struct Tile
 
 	public byte Value
 	{
-		get => (byte) (domain & 0x000F);
-		set => domain = (short) ((domain & 0xFFF0) | (value & 0x000F));
+		get => (byte) (backing & 0x000F);
+		set => backing = (short) ((backing & 0xFFF0) | (value & 0x000F));
 	}
 
 	public bool IsFixed
 	{
-		get => (domain & 0x0040) != 0;
-		private init => domain = (short) ((domain & 0xFFBF) | ((value ? 1 : 0) << 6));
+		get => (backing & 0x0040) != 0;
+		private init => backing = (short) ((backing & 0xFFBF) | ((value ? 1 : 0) << 6));
 	}
 	
-	public void ConstraintAdd(byte x) => domain |= (short) (1 << (16-x)); // Assumes that x < 10
-	public void ConstraintRemove(byte x) => domain &= (short) ~(1 << (16-x));
-	public bool ConstraintAny() => (domain & 0xFF80) != 0;
+	public void ConstraintAdd(byte x) => backing |= (short) (1 << (16-x)); // Assumes that x < 10
+	public void ConstraintRemove(byte x) => backing &= (short) ~(1 << (16-x));
+	public bool ConstraintAny() => (backing & 0xFF80) != 0;
 	public IEnumerable<byte> Constraint()
 	{
 		for (byte i = 1; i < 10; i++)
-			if ((domain & (1 << (16 - i))) != 0)
+			if ((backing & (1 << (16 - i))) != 0)
 				yield return i;
 	}
 
